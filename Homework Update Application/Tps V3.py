940e138ed52aebe0722e8c2b1effe91761c28ba5
ActoDado <96277722+ActoDado@users.noreply.github.com>
2023-02-19 00:31:14 +0800

    Add files via upload

---
+++ Homework Update Application/Tps V3.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+path1 = r"C:\Program Files (x86)\TpsFiles"
+
+sys.path.append(path1)
+
+from Uploading_Correction import *
+from PlaySong import *
+from Pretty import *
+
+mainfn()
+
+os.system('cls')
+#x = input('Thank you for using:)...')
